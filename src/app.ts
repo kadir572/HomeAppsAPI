847819f77ed989f452e07bf424c0157e3c767def
Kadir Karadavut <kadir9625@gmail.com>
2022-12-05 09:49:16 +0100

    allow all cors urls

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,10 @@ connectDB()
 
 const app = express()
 app.use(
-  cors({
-    origin: 'http://localhost:5173',
-  })
+  cors()
+  //   {
+  //   origin: 'http://localhost:5173',
+  // }
 )
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
